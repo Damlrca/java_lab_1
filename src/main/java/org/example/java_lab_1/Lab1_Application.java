8199a5672d04571761f5da89e6dea1e134c49648
Damlrca <sir.damirca@yandex.ru>
2024-02-25 18:42:35 +0300

    first commit

---
+++ src/main/java/org/example/java_lab_1/Lab1_Application.java
@@ -0,0 +1,23 @@
+package org.example.java_lab_1;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Lab1_Application extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Lab1_Application.class.getResource("main-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("lab1 : Меткий стрелок!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
